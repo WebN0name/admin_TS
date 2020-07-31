1ad3d6980597374c0aa1b581e0aaa8e2b7ead948
Pogozhev <pao00013@gmail.com>
2020-07-31 20:08:02 +0700

    123

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
 
 Vue.use(Vuex)
 
@@ -9,6 +10,14 @@ export default new Vuex.Store({
   mutations: {
   },
   actions: {
+
+    async Fetch_Data () {
+      axios.post('http://213.219.214.91:5000/fetch_msg_test', {
+        id: 0
+      }).then(r => {
+        console.log(r)
+      })
+    }
   },
   modules: {
   }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,6 +4,13 @@
       <v-card>
         <div class="header-block">
           <div class="table">
+            <div class="filters-wrap">
+              <div class="filters">
+                <v-autocomplete class="motive" placeholder="Rejection motive"></v-autocomplete>
+                <v-autocomplete class="code" placeholder="Code"></v-autocomplete>
+                <v-autocomplete class="region" placeholder="Region"></v-autocomplete>
+                <v-autocomplete class="ddc" placeholder="DDC"></v-autocomplete>
+              </div>
               <v-text-field
                 v-model="search"
                 append-icon="mdi-magnify"
@@ -12,6 +19,7 @@
                 hide-details
                 class="search"
               ></v-text-field>
+            </div>
               <v-spacer></v-spacer>
               <v-data-table
               :headers="headers"
@@ -24,27 +32,117 @@
           </div>
           <div class="buttons">
             <v-btn
+            class="notify_all custom"
             color="primary"
             >Notify all</v-btn>
             <v-btn
+            class="notify_select custom"
             color="primary"
-            >Notify select</v-btn>
+            >Notify selected</v-btn>
+            <v-btn
+                class="notify_all custom"
+                color="primary"
+              >Send reports</v-btn>
             <v-switch  label="Send SMS"></v-switch>
             <v-switch  label="Send Email"></v-switch>
-            <v-switch  label="Auto send at:"></v-switch>
+            <div class="setTime">
+            <v-switch  label="Auto send at:" height="74"></v-switch>
+            <v-overflow-btn
+              :items="dropdown_font"
+              label="07:00 AM"
+              target="#dropdown-example"
+              class="my-2"
+            ></v-overflow-btn>
+            </div>
           </div>
         </div>
       </v-card>
+        <div class="footer-block">
+          <div class="fields">
+            <div class="textfields">
+            <v-text-field
+              height="60px"
+              placeholder="Write E-mail to send..."
+            ></v-text-field>
+            <v-text-field
+              height="60px"
+              placeholder="Write Phone to send..."
+            ></v-text-field>
+            <!-- <label for="email">E-mail to send:</label>
+            <input type="text" class="email"> -->
+          </div>
+              <v-textarea
+                outlined
+                name="input-7-4"
+                height="120px"
+                label="Customer notification preview:"
+                no-resize="false"
+                value="..."
+              ></v-textarea>
+          </div>
+          <div class="send">
+            <div class="send-sms">
+              <v-text-field
+              height="60px"
+              placeholder="Write phone number..."
+              class="send-sms-field"
+            ></v-text-field>
+            <v-btn
+            class="send-sms-btn custom"
+            color="primary"
+            >Send SMS</v-btn>
+            </div>
+            <div class="getReported">
+                <v-btn
+                  class="notify_all custom"
+                  color="primary"
+                >import orders</v-btn>
+                <v-btn
+                class="notify_all custom"
+                color="primary"
+              >import customers</v-btn>
+              <v-btn
+                class="notify_all custom"
+                color="primary"
+              >import rej codes</v-btn>
+            </div>
+          </div>
+        </div>
     </div>
   </div>
 </template>
 
 <script>
-
+import { mapActions } from 'vuex'
 export default {
   name: 'Home',
 
   data: () => ({
+    dropdown_font: [
+      '00:00 AM',
+      '01:00 AM',
+      '02:00 AM',
+      '03:00 AM',
+      '04:00 AM',
+      '05:00 AM',
+      '06:00 AM',
+      '07:00 AM',
+      '08:00 AM',
+      '09:00 AM',
+      '10:00 AM',
+      '11:00 AM',
+      '00:00 PM',
+      '01:00 PM',
+      '02:00 PM',
+      '03:00 PM',
+      '04:00 PM',
+      '05:00 PM',
+      '06:00 PM',
+      '07:00 PM',
+      '08:00 PM',
+      '09:00 PM',
+      '10:00 PM',
+      '11:00 PM'],
     search: '',
     headers: [
       {
@@ -63,24 +161,24 @@ export default {
         value: 'rejCode'
       },
       {
-        text: 'First POC Nam',
+        text: 'POC',
         sortable: false,
         value: 'firstPOC'
       },
       {
-        text: 'First Owner',
+        text: 'Owner',
         sortable: false,
         value: 'firstOwner'
       },
       {
-        text: 'First phone number',
+        text: 'DDC',
         sortable: false,
-        value: 'firstPhone'
+        value: 'DDC'
       },
       {
-        text: 'First E-mail',
+        text: 'Region',
         sortable: false,
-        value: 'firstEmail'
+        value: 'region'
       },
       {
         text: 'Count SKU',
@@ -181,11 +279,33 @@ export default {
         countSKU: 6
       }
     ]
-  })
+  }),
+
+  async mounted(){
+    this.
+  }
+
 }
 </script>
 
 <style scoped>
+  .send-sms{
+    display: flex;
+    align-items: flex-end;
+  }
+  .fields{
+    width: 50%;
+    margin-right: 10px;
+  }
+  .send{
+    width: 50%;
+    margin-top: 60px;
+    margin-right: 10px;
+    margin-left: 10px;
+  }
+  .send-sms-field{
+    width: 90%;
+  }
   .home{
     max-width: 100vw;
     max-height: 100vh;
@@ -203,8 +323,8 @@ export default {
   }
 
   .search{
-    margin-top: 30px;
-    margin-bottom: 30px;
+    /* margin-top: 30px; */
+    /* margin-bottom: 30px; */
     width: 40%;
   }
 
@@ -233,5 +353,68 @@ export default {
     flex-direction: column;
     align-items: center;
     justify-content: center;
+  }
+
+  .footer-block{
+    display: flex;
+    min-width: 68.75rem;
+    width: 100%;
+  }
+  .textfields{
+    margin-top: 60px;
+    display: flex;
+    width: 98%;
+  }
+
+  .v-text-field{
+    padding: 5px;
+  }
+
+  .filters-wrap{
+    display: flex;
+    flex-direction: row;
+  }
+
+  .filters{
+    display: flex;
+    flex-direction: row;
+  }
+
+  .notify_select{
+    margin-top: 10px;
+  }
+
+  .send-sms-btn{
+    margin-bottom: 25px;
+    margin-left: 30px;
+  }
+
+  .getReported{
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  .left-side, .right-side{
+    width: 50%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-between;
+    height: 100px;
+  }
+  .setTime{
+    display: flex;
+    align-items: center;
+  }
+
+  .custom{
+    width: 200px;
+  }
+
+  .custom + .custom{
+    margin-top: 30px;
   }
 </style>
